2c42d2cd125eda6fd1d84a50341aead240976c5b
adnanabdalrahman <adnan.24150@gmail.com>
2025-05-18 21:37:32 +0200

    Frontend

---
+++ Frontend/out/_next/static/chunks/app/layout-94a57bfb8983ba5f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9810:function(n,e,u){Promise.resolve().then(u.t.bind(u,2148,23))},2148:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=9810)}),_N_E=n.O()}]);
